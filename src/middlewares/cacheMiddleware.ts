04d0204ec6678b6d113caa3dd1656a8a07f438e7
Samuel Wemimo <wemsamuel@gmail.com>
2024-08-14 22:26:26 -0700

    update: added pino logger for effective debugging

--- src/middlewares/cacheMiddleware.ts
+++ src/middlewares/cacheMiddleware.ts
@@ -1,10 +1,14 @@
 import type { Context } from 'hono';
 import type { ContextVariables } from '../constant';
+import mainLogger from '../loggers';
 
 interface CacheEntry {
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
   body: any;
   expiration: number;
 }
+
+const logger = mainLogger.child({ name: 'cacheMiddleware' });
 
 export const cacheMiddleware = () => {
   const cache = new Map<string, CacheEntry>();
@@ -18,21 +22,35 @@ export const cacheMiddleware = () => {
       cache: (body: object, expiration: number = 3600) => {
         const expireAt = Date.now() + expiration * 1000;
         const entry = { body, expiration: expireAt };
+        logger.info(
+          `Setting cache key: ${cacheKey}, to${JSON.stringify(entry)}`,
+        );
         cache.set(cacheKey, entry);
       },
       clear: () => {
+        logger.info(`Clearing cache key: ${cacheKey}`);
         cache.delete(cacheKey);
       },
       clearPath: (path: string) => {
         const fullKey = `${path}:${userId}`;
+        logger.info(`Clearing cache key: ${fullKey}`);
         cache.delete(fullKey);
       },
     });
     if (ctx.req.method.toLocaleUpperCase() === 'GET') {
       const cacheEntry = cache.get(cacheKey);
       if (cacheEntry) {
+        logger.info(
+          `Found cache entry: ${cacheKey}, to${JSON.stringify(cacheEntry)}`,
+        );
         if (cacheEntry.expiration > Date.now()) {
+          logger.info(
+            `return from key: ${cacheKey}, body: ${JSON.stringify(cacheEntry.body)}`,
+          );
           return ctx.json(cacheEntry.body);
+        } else {
+          logger.info(`
+            Cache entry expired cache key: ${cacheKey}, expiration: ${cacheEntry?.expiration}`);
         }
       }
     }
